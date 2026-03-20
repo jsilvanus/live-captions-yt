845737d97e52df2fc5fded7158e84011a62c54e1
Juha Itäleino <jsilvanus@gmail.com>
2026-03-20 15:04:47 +0200

    ffmpeg: wire DockerFfmpegRunner into factory

--- packages/lcyt-backend/src/ffmpeg/index.js
+++ packages/lcyt-backend/src/ffmpeg/index.js
@@ -1,13 +1,16 @@
 import { LocalFfmpegRunner } from './local-runner.js';
+import { DockerFfmpegRunner } from './docker-runner.js';
 
-export function createFfmpegRunner({ runner = 'spawn', ...opts } = {}) {
+export function createFfmpegRunner({ runner = process.env.FFMPEG_RUNNER || 'spawn', ...opts } = {}) {
   switch (runner) {
     case 'spawn':
     case 'local':
       return new LocalFfmpegRunner(opts);
+    case 'docker':
+      return new DockerFfmpegRunner(opts);
     default:
       throw new Error(`Unknown ffmpeg runner: ${runner}`);
   }
 }
 
-export { LocalFfmpegRunner };
+export { LocalFfmpegRunner, DockerFfmpegRunner };
