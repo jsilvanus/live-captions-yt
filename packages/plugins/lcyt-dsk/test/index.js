cdddccf6900f3eaa2b39d1fc2bc6636331e3e4ce
Juha Itäleino <jsilvanus@gmail.com>
2026-03-23 23:41:10 +0200

    test: add additional test imports for caption processor, editor auth, and DB helpers

--- packages/plugins/lcyt-dsk/test/index.js
+++ packages/plugins/lcyt-dsk/test/index.js
@@ -1,2 +1,5 @@
 import './dsk-templates-conflict.test.js';
 import './dsk-templates-autorenames.test.js';
+import './caption-processor-full.test.js';
+import './editor-auth.test.js';
+import './dsk-templates-db.test.js';
